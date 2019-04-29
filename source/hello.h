63f39110a80d6e16a2613ac22dd03602630fa4db
Alin M Elena <drFaustroll@circassia.home>
2019-04-29 11:25:03 +0100

    enable testing by default

---
+++ source/hello.h
@@ -0,0 +1,6 @@
+//    Copyright (c) 2015 Alin Marin Elena <alin@elena.space>
+//    The MIT License http://opensource.org/licenses/MIT
+#define Hello_VERSION_MAJOR 0
+#define Hello_VERSION_MINOR 0
+#define Hello_VERSION_PATCH 1
+#define Hello_VERSION_TWEAK 0
